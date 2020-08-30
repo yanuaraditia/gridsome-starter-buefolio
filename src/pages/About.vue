5717fe389ae196153975e5e83ddf49b5cabde2ad
Yanuar Aditia <yanuaraditia@outlook.com>
2020-08-30 17:09:58 +0700

    DIAMOND

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -60,7 +60,7 @@ export default {
   },
   metaInfo: {
     title: 'Me',
-    description: "I'm "+this.calculateAge()+" old now, working as Backend Developer using Laravel as the framework."
+    description: "I'm currently working as Backend Developer using Laravel as the framework."
   },
   computed:{
     calculateAge() {

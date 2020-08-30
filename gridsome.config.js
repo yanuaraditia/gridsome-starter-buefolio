582933b57941f298c53a955192ae4052c9762815
Yanuar Aditia <yanuaraditia@outlook.com>
2020-08-30 16:44:36 +0700

    TEST ENv

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Yanuar Aditia',
   plugins: [],
   testEnvironment: 'node'
 }
